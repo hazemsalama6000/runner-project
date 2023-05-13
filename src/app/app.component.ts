604319a01cd454b50b6e9f4af9c92f63ef833241
hazem <hazem.salama6000@outlook.com>
2023-05-13 22:39:52 +0300

    entries and list ending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'testapp';
+
+export class AppComponent implements OnInit {
+
+  constructor(private renderer:Renderer2){
+
+  }
+
+  ngOnInit(): void {
+    document.querySelector('form')?.addEventListener('submit', this.handleFormSubmit);
+  }
+
+  handleFormSubmit = (event: any)=>{
+    event.preventDefault();
+    let element = document.querySelector('#miles') as HTMLInputElement;
+    this.InList(element.value);
+    let form = document.getElementsByTagName('form')[0] as HTMLFormElement;
+    form.reset();
+  }
+
+  InList(val: any) {
+    let list = document.querySelector('#entries') as HTMLUListElement;
+    let liItem = document.createElement('li') as HTMLLIElement;
+    liItem.appendChild(document.createTextNode(val));
+    list.appendChild(liItem);
+    list.removeChild(list.firstElementChild as HTMLLIElement);
+  }
+
 }
