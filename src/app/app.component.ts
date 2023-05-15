ab8597eecbea0ed28de943848e5aa10b81453cb5
hazem <hazem.salama6000@outlook.com>
2023-05-15 22:02:45 +0300

    statics display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,54 @@ import { Component, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
 
 export class AppComponent implements OnInit {
 
-  constructor(private renderer:Renderer2){
+  entries: Array<number> = [];
 
+  constructor(private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
     document.querySelector('form')?.addEventListener('submit', this.handleFormSubmit);
   }
 
-  handleFormSubmit = (event: any)=>{
+  summer(total: any, currentval: any) {
+    return total + currentval;
+  }
+
+  calcTotal() {
+    let sum = this.entries.reduce(this.summer).toString();
+    document.getElementById("total")!.innerHTML = sum;
+    document.querySelector("#progressTotal")!.innerHTML = sum;
+  }
+
+  heigestVal() {
+    let max = Math.max(...this.entries);
+    document.querySelector("#height")!.innerHTML = max.toString();
+  }
+
+  calcAverage() {
+    document.querySelector("#average")!.innerHTML = String((this.entries.reduce(this.summer) / this.entries.length).toFixed(1));
+  }
+
+  handleFormSubmit = (event: any) => {
     event.preventDefault();
     let element = document.querySelector('#miles') as HTMLInputElement;
     this.InList(element.value);
+    this.entries.push(Number(element.value));
+
+    let call = new Promise((resolve, reject) => {
+      return resolve(this.calcTotal());
+    });
+
+    call.then((data: any) => {
+      this.calcAverage();
+      this.heigestVal();
+    });
+
     let form = document.getElementsByTagName('form')[0] as HTMLFormElement;
     form.reset();
   }
